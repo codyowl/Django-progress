99e20149f922bec8c9f7c1a5df9b46f76d324250
codyowl <codyowl@gmail.com>
2017-07-01 21:28:08 +0530

    A simple blog apps' schema added :heavy_plus_sign:

---
+++ model-schema-design/blog/blogapp/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BlogPost',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('content', models.TextField()),
+                ('posted', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='blogpost',
+            name='category',
+            field=models.ForeignKey(to='blogapp.Category'),
+        ),
+    ]
--- model-schema-design/blog/blogapp/models.py
+++ model-schema-design/blog/blogapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+#Just a simple blog app which will contains two models.
+#1.BlogPost(for getting blog details)
+#2.Category(for getting category details of the blog entries)
+
+class BlogPost(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	posted = models.DateTimeField(auto_now_add=True)
+	category = models.ForeignKey('Category')
+
+	def __unicode__(self):
+		return self.title
+
+class Category(models.Model):
+	title = models.CharField(max_length=200)	
+
+	def __unicode__(self):
+		return self.title
+		
