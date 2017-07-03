b0d37e215a5f306365fddbb974d4e59e4c624260
codyowl <codyowl@gmail.com>
2017-07-03 06:33:34 +0530

    Blog with comments added :heavy_plus_sign:

---
+++ model-schema-design/Blogwithcomments/comments/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('comment_body', models.TextField()),
+                ('comment_posted_by', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('content', models.TextField()),
+                ('posted_date', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='comments.Post'),
+        ),
+    ]
--- model-schema-design/Blogwithcomments/comments/models.py
+++ model-schema-design/Blogwithcomments/comments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	posted_date = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.title
+
+	#Adding a helper function to get all the comment for particular post
+	def get_all_comments(self):
+		comments = Comment.objects.filter(post=self)
+		return comments	
+
+class Comment(models.Model):
+	post = models.ForeignKey('Post')
+	comment_body = models.TextField()
+
+	def __unicode__(self):
+		return self.comment_body
