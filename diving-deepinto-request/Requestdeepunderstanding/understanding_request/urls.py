310e23b077785cfbcb42f97d3d8f1634bf02b960
codyowl <codyowl@gmail.com>
2017-07-04 06:00:52 +0530

    request.GET['param'] added :heavy_plus_sign:

--- diving-deepinto-request/Requestdeepunderstanding/understanding_request/urls.py
+++ diving-deepinto-request/Requestdeepunderstanding/understanding_request/urls.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('understanding_request.views',
 	url(r'^request-dot-GET/$','request_dot_GET', name='request_dot_GET'),
+	url(r'^request-dot-GET-get/$','request_dot_GET_dot_get', name='request_dot_GET_dot_get'),
+	url(r'^request-dot-GET-parameter/$','request_dot_GET_parameter', name='request_dot_GET_parameter'),
 	)
--- diving-deepinto-request/Requestdeepunderstanding/understanding_request/views.py
+++ diving-deepinto-request/Requestdeepunderstanding/understanding_request/views.py
@@ -4,6 +4,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def request_dot_GET(request, template='request_dot_GET.html'):
-	q_value = request.GET
-	print q_value
+	g_value = request.GET
+	print g_value
 	return render_to_response(template)
+
+def request_dot_GET_dot_get(request, template='request_dot_GET.html'):
+	g_value = request.GET.get('g')
+	print g_value
+	return render_to_response(template) 	
+
+def request_dot_GET_parameter(request, template='request_dot_GET.html'):
+	if 'g' in request.GET:
+		g_value = request.GET['g']
+	else:
+		g_value = False
+	
+	print g_value
+	return render_to_response(template)	
