556cf9ce1f1ad1c086d9516c7fa54baacc85c2be
codyowl <codyowl@gmail.com>
2017-07-05 08:12:57 +0530

    request.POST added to Requestunderstanding :heavy_plus_sign:

--- diving-deepinto-request/Requestdeepunderstanding/understanding_request/urls.py
+++ diving-deepinto-request/Requestdeepunderstanding/understanding_request/urls.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('understanding_request.views',
+	#request.GET
 	url(r'^request-dot-GET/$','request_dot_GET', name='request_dot_GET'),
 	url(r'^request-dot-GET-get/$','request_dot_GET_dot_get', name='request_dot_GET_dot_get'),
 	url(r'^request-dot-GET-parameter/$','request_dot_GET_parameter', name='request_dot_GET_parameter'),
+
+	#request.POST
+	url(r'^request-dot-POST/$','request_dot_POST', name='request_dot_POST'),
+	url(r'^request-dot-POST-get/$','request_dot_POST_dot_get', name='request_dot_POST_dot_get'),
+	url(r'^request-dot-POST-parameter/$','request_dot_POST_parameter', name='request_dot_POST_parameter'),
 	)
--- diving-deepinto-request/Requestdeepunderstanding/understanding_request/views.py
+++ diving-deepinto-request/Requestdeepunderstanding/understanding_request/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
 
 
 # Create your views here.
+#request.GET
 def request_dot_GET(request, template='request_dot_GET.html'):
 	g_value = request.GET
 	print g_value
@@ -18,6 +20,21 @@ def request_dot_GET_parameter(request, template='request_dot_GET.html'):
 		g_value = request.GET['g']
 	else:
 		g_value = False
-	
 	print g_value
-	return render_to_response(template)	
+	return render_to_response(template)
+
+#request.POSt
+def request_dot_POST(request, template='request_dot_POST.html'):
+	p_value = request.POST	
+	print p_value
+	return render_to_response(template, context_instance=RequestContext(request))	
+
+def request_dot_POST_dot_get(request, template='request_dot_POST.html'):
+	p_value = request.POST.get('p') #since p is the name of text box 	
+	print p_value
+	return render_to_response(template, context_instance=RequestContext(request))		
+
+def request_dot_POST_parameter(request, template='request_dot_POST.html'):
+	p_value = request.POST['p']#since p is the name of text box 	
+	print p_value
+	return render_to_response(template, context_instance=RequestContext(request))			
